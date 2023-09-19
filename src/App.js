600e24c735a5a7bc246a45d2a28dd02a0a613a43
josephenejimenez <jjosephene99@gmail.com>
2023-09-19 14:10:58 +0800

    [Done] Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-// import logo from './logo.svg';
-// import './App.css';
-import FunctionalComp from './Components/FunctionalComp';
-import {ClassComp1} from './Components/ClassComp';
-import Click from './Components/Click';
-import Counter from './Components/Counter';
+// Lesson: Components
+import FunctionalComp from './LComponents/Components/FunctionalComp';
+import {ClassComp1} from './LComponents/Components/ClassComp';
+import Click from './LComponents/Components/Click';
+import Counter from './LComponents/Components/Counter';
+
 
 function App() {
   return (
+    // Lesson: Components
    <div>
       <h1>Hello! Welcome to Simplilearn</h1>
       <h1>This video is about Components</h1>
